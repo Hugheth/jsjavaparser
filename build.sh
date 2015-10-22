412642603d14b614df551ad1852b2b25440ee59a
Andrew Moss <andrew@codekingdoms.com>
2015-10-22 10:22:46 +0100

    local peg

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
-# npm install pegjs umd
-pegjs -e "var parser" src/Java.1.7.pegjs lib/javaparser7.js
-pegjs src/Java.1.7.pegjs lib/javaparser7.node.js 
+#!/bin/bash
+
+set -o errexit
+
+npm install
+./node_modules/pegjs/bin/pegjs  -e "var parser" src/Java.1.7.pegjs lib/javaparser7.js
+./node_modules/pegjs/bin/pegjs  src/Java.1.7.pegjs lib/javaparser7.node.js
 # combine and output
 cat src/workerHeader.jsbit lib/javaparser7.js src/workerFooter.jsbit > lib/JavaParserWorker.js
