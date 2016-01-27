7acf3495ba8b5205fbaddd7c8535d1a04bd6221d
Hugheth <thehugheth@gmail.com>
2016-01-27 15:38:09 +0000

    Holy maccarooi

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 npm install
-./node_modules/pegjs/bin/pegjs  -e "var parser" src/Java.1.7.pegjs lib/javaparser7.js
-./node_modules/pegjs/bin/pegjs  src/Java.1.7.pegjs lib/javaparser7.node.js
+./node_modules/pegjs/bin/pegjs --cache -e "var parser" src/Java.1.7.pegjs lib/javaparser7.js
+./node_modules/pegjs/bin/pegjs --cache src/Java.1.7.pegjs lib/javaparser7.node.js
 # combine and output
 cat src/workerHeader.jsbit lib/javaparser7.js src/workerFooter.jsbit > lib/JavaParserWorker.js
