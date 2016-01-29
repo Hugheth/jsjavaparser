06f7218369310c0cf9c8d736ad84de62f1ded3ad
Andrew Moss <andrew@codekingdoms.com>
2016-01-29 09:44:05 +0000

    only build prod deps

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-npm install
+npm install --production
 ./node_modules/pegjs/bin/pegjs --cache -e "var parser" src/Java.1.7.pegjs lib/javaparser7.js
 ./node_modules/pegjs/bin/pegjs --cache src/Java.1.7.pegjs lib/javaparser7.node.js
 # combine and output
